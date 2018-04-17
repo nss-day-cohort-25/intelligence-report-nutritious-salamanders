dca92dffd1703df5bd9ebce736fb8373f3a31925
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-04-17 10:42:06 -0500

    Added database to localStorage in financeDB file  and loaded it into financeDOM file

---
+++ financeDB.js
@@ -0,0 +1,86 @@
+const financeDB = {}
+
+financeDB.bankAccts = []
+financeDB.businesses = []
+financeDB.blackMailed = []
+
+financeDB.bankAccts.push(
+    {
+     name: `Citibank`,
+     acct: 45321432234-32,
+     funds: 2.73  
+    },
+    {
+     name: `Bank of Ireland`,
+     acct: 75467429,
+     funds: 10.5  
+    },
+    {
+     name: `UBS`,
+     acct: 4332-8,
+     funds: 2.73  
+    },
+    {
+     name: `Bank of China`,
+     acct: 9992483-10,
+     funds: .750  
+    }    
+)
+// Worth represented in millions
+financeDB.businesses.push(
+    {
+     name: `Sunland Park Racetrack & Casino`,
+     location: `Las Cruces, NM`,
+     worth: 400,
+     laundered: .5  
+    },
+    {
+     name: `Nashville Car Wash & Detail`,
+     location: `Nashville, TN`,
+     worth: 8.2,
+     laundered: 2.6  
+  
+    },
+    {
+     name: `Baltimore Technologies`,
+     location: `Dublin, IR`,
+     worth: 400,
+     laundered: 1.25  
+  
+    },
+    {
+     name: `Uberfyt`,
+     location: `Silicon Valley, CA`,
+     worth: 30000,
+     laundered: 9.5  
+  
+    },
+
+)
+financeDB.blackMailed.push(
+    {
+     name: `Seán T. O'Kelly`,
+     position: `Vice President of the Irish Free State`
+    },
+    {
+     name: `Bill Haslam`,
+     position: `Governor of TN`
+    },
+    {
+     name: `Bill de Blasio`,
+     position: `Mayor of NYC`
+    },
+    {
+     name: `Huma Abedin`,
+     position: `Political Staffer`
+    }
+)
+
+
+const saveToDatabase = (databaseName, databaseObject) => {
+    const stringifiedDB = JSON.stringify(databaseObject)
+    console.log(stringifiedDB)
+    localStorage.setItem(databaseName, stringifiedDB)
+}
+
+saveToDatabase(`financialStmnts`, financeDB);
