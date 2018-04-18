b957b6456bad901b12d34bcc484270aa2e392356
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-18 09:33:31 -0500

    dashboard structure working

--- dashboardDB.js
+++ dashboardDB.js
@@ -1,9 +1,51 @@
-const allTasks = [];
+// Data for dashboard
+const dashboardData = [];
+
+const knownAssociates = [
+    {
+        name: 'John Smith',
+        age: 28,
+        location: 'United States'
+    },
+    {
+        name: 'Jennifer Strong',
+        age: 38,
+        location: 'South Africa'
+    },
+    {
+        name: 'Bob Westerson',
+        age: 91,
+        location: 'Mexico'
+    },
+    {
+        name: 'Amanda Frank',
+        age: 51,
+        location: 'Ireland'
+    },
+    {
+        name: 'Willie Billie',
+        age: 12,
+        location: 'Canada'
+    },
+]
+
+const countriesVisited = ['Ireland', 'United States', 'Mexico', 'Canada', 'Thailand'];
+const nicknames = ['Irish Carbomb', 'Gracie Girl', 'The Joker', 'Guinness', 'Grace Face'];
+
+dashboardData.push(knownAssociates);
+dashboardData.push(countriesVisited);
+dashboardData.push(nicknames);
+
 
 const saveToDatabase = (databaseName, databaseObject) => {
     const stringifiedDB = JSON.stringify(databaseObject)
-    console.log(stringifiedDB)
     localStorage.setItem(databaseName, stringifiedDB)
 }
 
-saveToDatabase('tasks', allTasks);
+const loadDatabase = () => {
+    const databaseString = localStorage.getItem('dashboard');
+    let databaseData =  JSON.parse(databaseString) 
+    return databaseData;
+  }
+
+saveToDatabase('dashboard', dashboardData);
