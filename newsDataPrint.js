1e3dabfd37995f3c4ed9f2b3ad4935499601f2e7
Levi Schubert <me@levischubert.com>
2018-04-18 10:39:01 -0500

    adjusting print and styles

---
+++ newsDataPrint.js
@@ -0,0 +1,63 @@
+
+
+const newsPrint = () =>{
+	currentNewsData.events.forEach(currentEvent => {
+
+		const card = document.createElement("section")
+		card.classList = "card event"
+
+		const topline = document.createElement("h2")
+		topline.classList = "title"
+
+		const headline = document.createElement("span")
+		headline.textContent = currentEvent.headline
+		topline.appendChild(headline)
+
+		const rating = document.createElement("span")
+		rating.classList = "rating"
+		rating.textContent = currentEvent.rating
+		topline.appendChild(rating)
+
+		card.appendChild(topline)
+
+		const date = document.createElement("p")
+		date.textContent = currentEvent.date
+		card.appendChild(date)
+
+		const location = document.createElement("p")
+		location.textContent = currentEvent.location
+		card.appendChild(location)
+
+		const desc = document.createElement("p")
+		desc.textContent = currentEvent.description
+		card.appendChild(desc)
+
+		const victim = document.createElement("p")
+		victim.textContent = "Victims: "
+		currentEvent.victims.forEach(iter => {
+			console.log(iter)
+			victim.textContent += (iter + ", ")
+			
+		})		
+		card.appendChild(victim)
+
+		const witness = document.createElement("p")
+		witness.textContent = "Witnesses: "
+		currentEvent.witnesses.forEach(iter => {
+			witness.textContent += (iter + ", ")
+			
+		})		
+		card.appendChild(witness)
+
+		const law = document.createElement("p")
+		law.textContent = currentEvent.law
+		card.appendChild(law)
+
+		fragment.appendChild(card)
+	})
+
+	newsRef.appendChild(fragment)
+	
+}
+
+// newsBuilder()
